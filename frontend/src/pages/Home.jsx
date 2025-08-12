bd3372ac335403a00d4b4a6fd45514c5ec83a75e
Wilmari Oosthuizen <24935603@sun.ac.za>
2025-08-12 23:31:18 +0200

    home page basic UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,39 @@
+import React, { useState } from "react";
+import './Home.css';
+import {useNavigate} from "react-router-dom";
+
 export default function Home() {
-  return <h1>Home Page</h1>;
+   const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate('/Login'); 
+  };
+  const handleSend = () => {
+    navigate('/Send');
+  };
+  const handleTopup = () => {
+    navigate('/Topup');
+  };
+
+  return (
+       <div className="home">
+         <h2>Home</h2>
+         <div className="account">
+            <span className="account-info">Logged in as: </span>
+         </div>
+         
+         <div className="balance-section">
+           <span>Your Balance:</span>
+         </div>
+         
+         <div className="button-section">
+         <button onClick={handleSend}>Send </button>
+         <button onClick={handleTopup}>Topup</button>
+         </div> 
+         <div className="logout-button">
+         <button onClick={handleLogout}>Logout</button>
+         </div>
+
+        </div>    
+  );
 }
