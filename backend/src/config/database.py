1f161d2e74cdd326161adb367fc443ef14d3665f
Marnich Jacques Steyn <27296660@sun.ac.za>
2025-08-12 17:40:54 +0200

    made database.py for setting up db locally (have not added alembic yet) aswell as sample files for the table containing clients (Clients.py) and init.py

---
+++ backend/src/config/database.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from pathlib import Path
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+#finding db url in .env file
+ROOT_DIRECTORY = Path(__file__).resolve().parents[2]
+load_dotenv(ROOT_DIRECTORY / ".env")
+
+DB_URL = os.getenv("DATABASE_URL")
+
+if not (DB_URL) : 
+    raise RuntimeError("no set DATABASE_URL in .env file")
+
+#database engine, pool_pre_ping used to avoid stale connection
+db_engine = create_engine(DB_URL, pool_pre_ping=True)
+LocalSession = sessionmaker(autoflush=False, autocommit=False, bind=db_engine)
+
+#Base model for models in model.py (Alembic)
+SPBase = declarative_base()
+
+def get_db(): 
+    db = LocalSession
+    try:
+        yield db
+    finally:
+        db.close()
