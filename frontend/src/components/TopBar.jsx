b7684297788ca90a0f2a094e75692dc3b284d54d
Sias Johan Van Der Linde <26907526@sun.ac.za>
2025-09-10 11:36:16 +0200

    Updated topbar for unlogged in users, added navigation to landing page

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -7,12 +7,30 @@ export default function TopBar({ user }) {
     localStorage.removeItem('currentUser');
     navigate('/login');
   };
+
+  const handleLogin = () => {
+    navigate('/login');
+  };
+
   return (
     <header className="topBar">
       <div className="logo" onClick={() => navigate('/dashboard')} style={{fontWeight:600}}>SafePay+</div>
       <div className="topbar-right">
         {user && <span className="user-name">{user.name}</span>}
-        <button className="btn" onClick={handleLogout}>Logout</button>
+        {user ? (
+          <button className="btn" onClick={handleLogout}>
+            Logout
+          </button>
+        ) : (
+          <div>
+            <button className="btn-signin" onClick={handleLogin}>
+              Sign in
+            </button>
+            <button className="btn" onClick={handleLogin}>
+              Sign up
+            </button>
+          </div>
+        )}
       </div>
     </header>
   );
